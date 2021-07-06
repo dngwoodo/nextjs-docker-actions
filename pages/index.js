6d942f7fceb384df07133d5cc2f99e40a32f7856
dngwoodo <dngwoodo@gmail.com>
2021-07-06 15:57:19 +0900

    Update content

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          yugesh Welcome to <a href="https://nextjs.org">Next.js!</a>
+          1231231238127389217389179 Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
